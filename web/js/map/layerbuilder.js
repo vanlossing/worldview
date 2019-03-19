d0381f247087121de45ee1a0d3f4de6768daef28
ZachTRice <zachtrice@gmail.com>
2019-03-19 13:29:47 -0400

    Change style endpoint in layer files and update to OL6

--- web/js/map/layerbuilder.js
+++ web/js/map/layerbuilder.js
@@ -15,6 +15,8 @@ import lodashCloneDeep from 'lodash/cloneDeep';
 import lodashMerge from 'lodash/merge';
 import lodashEach from 'lodash/each';
 import { lookupFactory } from '../ol/lookupimagetile';
+import OlCluster from 'ol/source/Cluster';
+import { Circle as CircleStyle, Fill, Stroke, Style, Text } from 'ol/style.js';
 
 export function mapLayerBuilder(models, config, cache, mapUi) {
   var self = {};
@@ -341,12 +343,36 @@ export function mapLayerBuilder(models, config, cache, mapUi) {
       })
     });
 
+    // var clusterSource = new OlCluster({
+    //   distance: parseInt(20, 10),
+    //   source: sourceOptions
+    // });
+
+    // var styleCache = {};
+
+    // var styles = function(feature) {
+    //   var style = styleCache['default'];
+    //   if (!style) {
+    //     style = new Style({
+    //       image: new CircleStyle({
+    //         radius: 5,
+    //         fill: new Fill({
+    //           color: '#ff0000'
+    //         })
+    //       })
+    //     });
+    //     styleCache['default'] = style;
+    //   }
+    //   return style;
+    // };
+
     var layer = new LayerVectorTile({
       renderMode: 'image',
-      renderBuffer: '100',
+      renderBuffer: '5',
       extent: extent,
       source: sourceOptions,
       declutter: true
+      // style: styles
     });
 
     if (config.vectorStyles && def.vectorStyle.id) {
@@ -354,18 +380,21 @@ export function mapLayerBuilder(models, config, cache, mapUi) {
       var vectorStyle = def.vectorStyle.id;
       var glStyle = vectorStyles[vectorStyle];
 
+      console.log(vectorStyles);
+      console.log(vectorStyle);
+
       $(document).ready(function() {
         $(document).on('change', document.getElementById('frpCheckbox'), function(e) {
           if (document.getElementById('frpCheckbox').checked === true) {
-            applyStyle(layer, glStyle, 'MODIS_Fire_Points_FRP', matrixSet.resolutions);
+            stylefunction(layer, glStyle, 'MODIS_Fire_Points_FRP', matrixSet.resolutions);
           } else if (document.getElementById('confidenceCheckbox').checked === true) {
-            applyStyle(layer, glStyle, 'MODIS_Fire_Points_Confidence', matrixSet.resolutions);
+            stylefunction(layer, glStyle, 'MODIS_Fire_Points_Confidence', matrixSet.resolutions);
           } else {
-            applyStyle(layer, glStyle, 'default_style', matrixSet.resolutions);
+            stylefunction(layer, glStyle, 'default_style', matrixSet.resolutions);
           }
         });
-        applyStyle(layer, glStyle, 'default_style', matrixSet.resolutions);
       });
+      stylefunction(layer, glStyle, 'default_style', matrixSet.resolutions);
     }
 
     return layer;
